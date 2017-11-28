8331a0bf1fa877fc9e9b5efc3a6727ee2f5a8d84
nraovic <n.raovic@gmail.com>
2017-11-28 19:41:24 +0100

    style: Add comments

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ function shuffle(array) {
     }
 }
 
-//reload the page when the element with elementClass is clicked
+//reload the page when element with elementClass is clicked
 const reload = function (elementClass) {
     const element = document.querySelector(elementClass);
     element.addEventListener('click', (e) => {
@@ -138,10 +138,6 @@ ul.addEventListener('click', (e) => {
         return;
     }
     showCards(li);
-
-    const s = function() {
-        li.className = "";
-    }
     //if the number of opened cards is 2, check if the cards match and update number of moves and stars
     if (openCards.length != 2) {
         return;
@@ -155,38 +151,3 @@ ul.addEventListener('click', (e) => {
 
 //reload the page when the restart symbol is clicked
 reload('.restart');
-
-/*
-const restart = document.querySelector('.restart');
-const playBtn = document.
-restart.addEventListener('click', (e) => {
-    location.reload();
-})
-
-
-button.addEventListener('clicked', (e) => {
-    location.reload();
-})
-*/
-
-/*
- * Create a list that holds all of your cards
- */
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
